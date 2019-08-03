d6a8885bca801ad2b46b05d6ba13862811676213
Daniel de Koning <daniel.de.koning@gmail.com>
2019-08-03 12:06:45 +0200

    Import classes into init file

---
+++ activity_browser/app/ui/wizards/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .db_import_wizard import (CopyDatabaseDialog, DatabaseImportWizard,
+                               DefaultBiosphereDialog)
+from .settings_wizard import SettingsWizard
