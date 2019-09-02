cd0f2cb7775e71deb7c26cf5410341c95fa315c4
dgdekoning <daniel.de.koning@gmail.com>
2019-09-02 15:16:05 +0200

    Add formula fields and create `get_usable_parameters` method for
    exchange tables

--- activity_browser/app/ui/tables/activity.py
+++ activity_browser/app/ui/tables/activity.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from bw2data.parameters import (ProjectParameter, DatabaseParameter, Group,
+                                ActivityParameter)
 import pandas as pd
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -170,15 +172,56 @@ class BaseExchangeTable(ABDataFrameEdit):
         signals.open_activity_tab.emit(key)
         signals.add_activity_to_history.emit(key)
 
+    def get_usable_parameters(self) -> list:
+        """ Use the `key` set for the table to determine the database and
+        group of the activity, constrain usable parameters.
+        """
+        project = [
+            [p.name, p.amount, "project"] for p in ProjectParameter.select()
+        ]
+        if self.key is None:
+            return project
+
+        query = (DatabaseParameter.select()
+                 .where(DatabaseParameter.database == self.key[0]))
+        database = [
+            [p.name, p.amount, "database"] for p in query.iterator()
+        ]
+
+        try:
+            # Get the parameter for the current activity
+            act = (ActivityParameter
+                   .get(ActivityParameter.database == self.key[0],
+                        ActivityParameter.code == self.key[1]))
+            # First, build a list for parameters in the same group
+            query = (ActivityParameter.select()
+                     .where(ActivityParameter.group == act.group))
+            activity = [
+                [p.name, p.amount, "activity"] for p in query.iterator()
+            ]
+            # Then extend the list with parameters from groups in the `order`
+            # field
+            group = Group.get(name=act.group)
+            query = (ActivityParameter.select()
+                     .where(ActivityParameter.group.in_(group.order)))
+            activity += [
+                [p.name, p.amount, "activity"] for p in query.iterator()
+            ]
+        except ActivityParameter.DoesNotExist:
+            activity = []
+
+        return project + database + activity
+
 
 class ProductExchangeTable(BaseExchangeTable):
-    COLUMNS = ["Amount", "Unit", "Product"]
+    COLUMNS = ["Amount", "Unit", "Product", "Formula"]
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setItemDelegateForColumn(0, FloatDelegate(self))
         self.setItemDelegateForColumn(1, StringDelegate(self))
         self.setItemDelegateForColumn(2, StringDelegate(self))
+        self.setItemDelegateForColumn(3, FormulaDelegate(self))
         self.setDragDropMode(QtWidgets.QTableView.DragDrop)
         self.table_name = "product"
 
@@ -186,13 +229,14 @@ class ProductExchangeTable(BaseExchangeTable):
         row, adj_act = super().create_row(exchange)
         row.update({
             "Product": adj_act.get("reference product") or adj_act.get("name"),
+            "Formula": exchange.get("formula")
         })
         return row, adj_act
 
     def _resize(self) -> None:
         """ Ensure the `exchange` column is hidden whenever the table is shown.
         """
-        self.setColumnHidden(3, True)
+        self.setColumnHidden(4, True)
 
     def dragEnterEvent(self, event):
         """ Accept exchanges from a technosphere database table, and the
@@ -267,7 +311,7 @@ class BiosphereExchangeTable(BaseExchangeTable):
         self.setItemDelegateForColumn(3, ViewOnlyDelegate(self))
         self.setItemDelegateForColumn(4, ViewOnlyDelegate(self))
         self.setItemDelegateForColumn(5, ViewOnlyDelegate(self))
-        self.setItemDelegateForColumn(6, StringDelegate(self))
+        self.setItemDelegateForColumn(6, FormulaDelegate(self))
         self.table_name = "biosphere"
         self.setDragDropMode(QtWidgets.QTableView.DropOnly)
 
