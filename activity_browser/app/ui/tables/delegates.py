e6b5075d2193fbe141fbcb660eec06d88bfd3972
dgdekoning <daniel.de.koning@gmail.com>
2019-09-03 09:32:31 +0200

    Give up on importing each specific PyQt class

--- activity_browser/app/ui/tables/delegates.py
+++ activity_browser/app/ui/tables/delegates.py
@@ -3,75 +3,70 @@ from itertools import zip_longest
 from typing import List
 
 import brightway2 as bw
-from PyQt5.QtCore import QAbstractItemModel, QLocale, QModelIndex, Qt
-from PyQt5.QtGui import QDoubleValidator, QStandardItem, QStandardItemModel
-from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog, QLineEdit,
-                             QStyle, QStyledItemDelegate, QDialogButtonBox,
-                             QFormLayout, QListView,QStyleOptionButton,
-                             QWidget)
+from PyQt5 import QtCore, QtGui, QtWidgets
 from stats_arrays import uncertainty_choices
 
 
-class FloatDelegate(QStyledItemDelegate):
+class FloatDelegate(QtWidgets.QStyledItemDelegate):
     """ For managing and validating entered float values.
     """
     def __init__(self, parent=None):
         super().__init__(parent)
 
     def createEditor(self, parent, option, index):
-        editor = QLineEdit(parent)
-        locale = QLocale(QLocale.English)
-        locale.setNumberOptions(QLocale.RejectGroupSeparator)
-        validator = QDoubleValidator()
+        editor = QtWidgets.QLineEdit(parent)
+        locale = QtCore.QLocale(QtCore.QLocale.English)
+        locale.setNumberOptions(QtCore.QLocale.RejectGroupSeparator)
+        validator = QtGui.QDoubleValidator()
         validator.setLocale(locale)
         editor.setValidator(validator)
         return editor
 
-    def setEditorData(self, editor: QLineEdit, index: QModelIndex):
+    def setEditorData(self, editor: QtWidgets.QLineEdit, index: QtCore.QModelIndex):
         """ Populate the editor with data if editing an existing field.
         """
-        data = index.data(Qt.DisplayRole)
+        data = index.data(QtCore.Qt.DisplayRole)
         value = float(data) if data else 0
         editor.setText(str(value))
 
-    def setModelData(self, editor: QLineEdit, model: QAbstractItemModel,
-                     index: QModelIndex):
+    def setModelData(self, editor: QtWidgets.QLineEdit, model: QtCore.QAbstractItemModel,
+                     index: QtCore.QModelIndex):
         """ Take the editor, read the given value and set it in the model
         """
         try:
             value = float(editor.text())
-            model.setData(index, value, Qt.EditRole)
+            model.setData(index, value, QtCore.Qt.EditRole)
         except ValueError:
             pass
 
 
-class StringDelegate(QStyledItemDelegate):
+class StringDelegate(QtWidgets.QStyledItemDelegate):
     """ For managing and validating entered string values.
     """
     def __init__(self, parent=None):
         super().__init__(parent)
 
     def createEditor(self, parent, option, index):
-        editor = QLineEdit(parent)
+        editor = QtWidgets.QLineEdit(parent)
         return editor
 
-    def setEditorData(self, editor: QLineEdit, index: QModelIndex):
+    def setEditorData(self, editor: QtWidgets.QLineEdit, index: QtCore.QModelIndex):
         """ Populate the editor with data if editing an existing field.
         """
-        value = index.data(Qt.DisplayRole)
+        value = index.data(QtCore.Qt.DisplayRole)
         # Avoid setting 'None' type value as a string
         value = str(value) if value else ""
         editor.setText(value)
 
-    def setModelData(self, editor: QLineEdit, model: QAbstractItemModel,
-                     index: QModelIndex):
+    def setModelData(self, editor: QtWidgets.QLineEdit, model: QtCore.QAbstractItemModel,
+                     index: QtCore.QModelIndex):
         """ Take the editor, read the given value and set it in the model
         """
         value = editor.text()
-        model.setData(index, value, Qt.EditRole)
+        model.setData(index, value, QtCore.Qt.EditRole)
 
 
-class DatabaseDelegate(QStyledItemDelegate):
+class DatabaseDelegate(QtWidgets.QStyledItemDelegate):
     """ Nearly the same as the string delegate, but presents as
     a combobox menu containing the databases of the current project.
     """
@@ -79,25 +74,25 @@ class DatabaseDelegate(QStyledItemDelegate):
         super().__init__(parent)
 
     def createEditor(self, parent, option, index):
-        editor = QComboBox(parent)
+        editor = QtWidgets.QComboBox(parent)
         editor.insertItems(0, bw.databases.list)
         return editor
 
-    def setEditorData(self, editor: QComboBox, index: QModelIndex):
+    def setEditorData(self, editor: QtWidgets.QComboBox, index: QtCore.QModelIndex):
         """ Populate the editor with data if editing an existing field.
         """
-        value = str(index.data(Qt.DisplayRole))
+        value = str(index.data(QtCore.Qt.DisplayRole))
         editor.setCurrentText(value)
 
-    def setModelData(self, editor: QComboBox, model: QAbstractItemModel,
-                     index: QModelIndex):
+    def setModelData(self, editor: QtWidgets.QComboBox, model: QtCore.QAbstractItemModel,
+                     index: QtCore.QModelIndex):
         """ Take the editor, read the given value and set it in the model.
         """
         value = editor.currentText()
-        model.setData(index, value, Qt.EditRole)
+        model.setData(index, value, QtCore.Qt.EditRole)
 
 
-class CheckboxDelegate(QStyledItemDelegate):
+class CheckboxDelegate(QtWidgets.QStyledItemDelegate):
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -111,16 +106,16 @@ class CheckboxDelegate(QStyledItemDelegate):
         https://stackoverflow.com/a/11778012
         https://stackoverflow.com/q/15235273
         """
-        value = bool(index.data(Qt.DisplayRole))
-        button = QStyleOptionButton()
-        button.state = QStyle.State_Enabled
-        button.state |= QStyle.State_Off if not value else QStyle.State_On
+        value = bool(index.data(QtCore.Qt.DisplayRole))
+        button = QtWidgets.QStyleOptionButton()
+        button.state = QtWidgets.QStyle.State_Enabled
+        button.state |= QtWidgets.QStyle.State_Off if not value else QtWidgets.QStyle.State_On
         button.rect = option.rect
         # button.text = "False" if not value else "True"  # This also adds text
-        QApplication.style().drawControl(QStyle.CE_CheckBox, button, painter)
+        QtWidgets.QApplication.style().drawControl(QtWidgets.QStyle.CE_CheckBox, button, painter)
 
 
-class ViewOnlyDelegate(QStyledItemDelegate):
+class ViewOnlyDelegate(QtWidgets.QStyledItemDelegate):
     """ Disable the editor functionality to allow specific columns of an
     editable table to be view-only.
     """
@@ -131,21 +126,21 @@ class ViewOnlyDelegate(QStyledItemDelegate):
         return None
 
 
-class OrderedListInputDialog(QDialog):
+class OrderedListInputDialog(QtWidgets.QDialog):
     """ Mostly cobbled together from: https://stackoverflow.com/a/41310284
     and https://stackoverflow.com/q/26936585
     """
-    def __init__(self, parent=None, flags=Qt.Window):
+    def __init__(self, parent=None, flags=QtCore.Qt.Window):
         super().__init__(parent=parent, flags=flags)
         self.setWindowTitle("Select and order items")
 
-        form = QFormLayout(self)
-        self.list_view = QListView(self)
-        self.list_view.setDragDropMode(QListView.InternalMove)
+        form = QtWidgets.QFormLayout(self)
+        self.list_view = QtWidgets.QListView(self)
+        self.list_view.setDragDropMode(QtWidgets.QListView.InternalMove)
         form.addRow(self.list_view)
-        model = QStandardItemModel(self.list_view)
+        model = QtGui.QStandardItemModel(self.list_view)
         self.list_view.setModel(model)
-        buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttons = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel)
         form.addRow(buttons)
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
@@ -164,7 +159,7 @@ class OrderedListInputDialog(QDialog):
         model = self.list_view.model()
         model.clear()
         for i, checked in items:
-            item = QStandardItem(i)
+            item = QtGui.QStandardItem(i)
             item.setCheckable(True)
             item.setCheckState(checked)
             model.appendRow(item)
@@ -179,7 +174,7 @@ class OrderedListInputDialog(QDialog):
         return selected
 
 
-class ListDelegate(QStyledItemDelegate):
+class ListDelegate(QtWidgets.QStyledItemDelegate):
     """ For managing and validating entered string values
     https://stackoverflow.com/a/40275439
     """
@@ -187,8 +182,8 @@ class ListDelegate(QStyledItemDelegate):
         super().__init__(parent)
 
     def createEditor(self, parent, option, index):
-        editor = QWidget(parent)
-        dialog = OrderedListInputDialog(editor, Qt.Window)
+        editor = QtWidgets.QWidget(parent)
+        dialog = OrderedListInputDialog(editor, QtCore.Qt.Window)
 
         # Check which table is asking for a list
         if hasattr(parent, "table_name") and parent.table_name == "activity_parameter":
@@ -198,11 +193,11 @@ class ListDelegate(QStyledItemDelegate):
 
         return editor
 
-    def setEditorData(self, editor: QWidget, index: QModelIndex):
+    def setEditorData(self, editor: QtWidgets.QWidget, index: QtCore.QModelIndex):
         """ Populate the editor with data if editing an existing field.
         """
         dialog = editor.findChild(OrderedListInputDialog)
-        value = index.data(Qt.DisplayRole)
+        value = index.data(QtCore.Qt.DisplayRole)
         if value:
             value_list = [i.lstrip() for i in value.split(",")]
         else:
@@ -215,16 +210,16 @@ class ListDelegate(QStyledItemDelegate):
             checked = dialog.add_items_value(value_list, True)
             dialog.set_items(checked + unchecked)
 
-    def setModelData(self, editor: QWidget, model: QAbstractItemModel,
-                     index: QModelIndex):
+    def setModelData(self, editor: QtWidgets.QWidget, model: QtCore.QAbstractItemModel,
+                     index: QtCore.QModelIndex):
         """ Take the editor, read the given value and set it in the model
         """
         dialog = editor.findChild(OrderedListInputDialog)
         value = ", ".join(map(lambda i: str(i), dialog.items_selected()))
-        model.setData(index, value, Qt.EditRole)
+        model.setData(index, value, QtCore.Qt.EditRole)
 
 
-class UncertaintyDelegate(QStyledItemDelegate):
+class UncertaintyDelegate(QtWidgets.QStyledItemDelegate):
     """ A combobox containing the sorted list of possible uncertainties
     `setModelData` stores the integer id of the selected uncertainty
     distribution.
@@ -240,12 +235,12 @@ class UncertaintyDelegate(QStyledItemDelegate):
         Note that the `choices` attribute of uncertainty_choices is already
         sorted by id.
         """
-        editor = QComboBox(parent)
+        editor = QtWidgets.QComboBox(parent)
         items = sorted(self.choices, key=self.choices.get)
         editor.insertItems(0, items)
         return editor
 
-    def setEditorData(self, editor: QComboBox, index: QModelIndex):
+    def setEditorData(self, editor: QtWidgets.QComboBox, index: QtCore.QModelIndex):
         """ Lookup the description text set in the model using the reverse
         dictionary for the uncertainty choices.
 
@@ -253,12 +248,12 @@ class UncertaintyDelegate(QStyledItemDelegate):
         description of the uncertainty distribution), so we cannot simply
         take the value and set the index in that way.
         """
-        value = index.data(Qt.DisplayRole)
+        value = index.data(QtCore.Qt.DisplayRole)
         editor.setCurrentIndex(self.choices.get(value, 0))
 
-    def setModelData(self, editor: QComboBox, model: QAbstractItemModel,
-                     index: QModelIndex):
+    def setModelData(self, editor: QtWidgets.QComboBox, model: QtCore.QAbstractItemModel,
+                     index: QtCore.QModelIndex):
         """ Read the current index of the combobox and return that to the model.
         """
         value = editor.currentIndex()
-        model.setData(index, value, Qt.EditRole)
+        model.setData(index, value, QtCore.Qt.EditRole)
