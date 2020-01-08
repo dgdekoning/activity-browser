5f861df4d441054244c9a8aaa2b4d846754e56d8
dgdekoning <daniel.de.koning@gmail.com>
2020-01-08 14:22:12 +0100

    Use of empty QWidget to hang QWizard on means slight change
    to the test is required.

--- tests/test_import_wizard.py
+++ tests/test_import_wizard.py
@@ -7,11 +7,14 @@ def test_open_db_wizard(qtbot, ab_app):
     assert bw.projects.current == 'pytest_project'
     qtbot.waitForWindowShown(ab_app.main_window)
     project_tab = ab_app.main_window.left_panel.tabs['Project']
+
+    def handle_message():
+        box = QtWidgets.QApplication.activeWindow()
+        btn = box.button(QtWidgets.QWizard.CancelButton)
+        qtbot.mouseClick(btn, QtCore.Qt.LeftButton, delay=1)
+
+    QtCore.QTimer.singleShot(100, handle_message)
     qtbot.mouseClick(
         project_tab.databases_widget.import_database_button,
-        QtCore.Qt.LeftButton
-    )
-    qtbot.mouseClick(
-        ab_app.controllers.database.db_wizard.button(QtWidgets.QWizard.CancelButton),
         QtCore.Qt.LeftButton
     )
